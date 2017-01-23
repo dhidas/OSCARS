e4fe62b875857445deedd8c0ae5ff834f608b991
Dean Andrew Hidas <dhidas@login01.osgconnect.net>
2017-01-23 10:27:32 -0600

    fix for .sh inputs

--- osg/osg_run_Example_122.sh
+++ osg/osg_run_Example_122.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+Process=$1
+NAME=$2
+
+
 # Load same modules you used to compile oscars
 module load gcc/4.9.2
 module load python/3.5.2
 
-python examples/Example_122_OSG.py $SECTION $NAME
+python examples/Example_122_OSG.py $Process $NAME
