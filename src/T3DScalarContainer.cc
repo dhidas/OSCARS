a27d8e50d9381508f02e4c4017d9c50de68caa7b
dhidas <dhidas@dhidas.com>
2017-02-22 14:13:09 -0500

    Error message points to file causing error

--- src/T3DScalarContainer.cc
+++ src/T3DScalarContainer.cc
@@ -115,7 +115,7 @@ void T3DScalarContainer::AverageFromFilesText (std::vector<std::string> const& F
 
           // This must be file index 0
           if (i != 0) {
-            throw std::length_error("Inconsistent file formats found");
+            throw std::length_error("Inconsistent file format found in file " + Files[i]);
           }
 
           break;
@@ -151,7 +151,7 @@ void T3DScalarContainer::AverageFromFilesText (std::vector<std::string> const& F
 
           // This must be file index 0
           if (i != 0) {
-            throw std::length_error("Inconsistent file formats found");
+            throw std::length_error("Inconsistent file format found in file " + Files[i]);
           }
 
           break;
