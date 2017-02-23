31bc9d4935c77184efa69aa1f68126fc0bd973a1
dhidas <dhidas@dhidas.com>
2017-02-23 16:46:11 -0500

    added R calculation

--- test.py
+++ test.py
@@ -4,5 +4,6 @@ oth = oscars.th.th()
 
 print(oth.undulator_K(1, 0.055))
 
-# We will put a new function here
-# We will put a new function here
+
+
+oth.dipole_spectrum(bfield=0.4, beam_energy_GeV=3, angle=0.0001, energy_range_eV=[10, 1000])
