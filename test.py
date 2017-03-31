a5e39385b952d5ff03c377858d6d5d57c6a80b5f
Julia <hindsjr@appstate.edu>
2017-03-31 16:22:55 -0400

    updating test

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ print(oth.undulator_K(1, 0.055))
 #result = oth.dipole_spectrum(bfield=0.4, beam_energy_GeV=3, angle=0.0001, energy_eV=2390)
 
 
-result = oth.undulator_flux(bfield=0.8, period=0.022, nperiods=41, beam_energy_GeV=3, angle_vertical=-0.0001, angle_horizontal=0.0001, energy_eV=2390)
+result = oth.undulator_flux_k(k=1, period=0.033, nperiods=41, beam_energy_GeV=3, harmonic=1)
