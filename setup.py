ee1566bafb5a2bd58e246b0624cb64eacfa8c2de
dhidas <dhidas@dhidas.com>
2017-05-10 12:08:55 -0400

    update dev version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ moduleOSCARSTH = Extension('oscars.th',
 
 setup(
   name="oscars",
-  version="1.36.01",
+  version="1.36.03",
   description = 'Open Source Code for Advanced Radiation Simulation',
   author = 'Dean Andrew Hidas',
   author_email = 'dhidas@bnl.gov',
