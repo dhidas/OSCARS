9cc1225725cc9065b4f7f0cc5e679e1bd3e7a0ea
Hidas <dhidas@id01.ags.bnl.gov>
2017-05-12 11:42:13 -0400

    test for setup distutils gpu lib

---
+++ setup_gpu.py
@@ -0,0 +1,75 @@
+import os
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
+
+moduleOSCARSSR = Extension('oscars.sr',
+                      include_dirs = ['include'],
+                      sources = ['src/OSCARSSR.cc',
+                                 'src/OSCARSSR_Python.cc',
+                                 'src/T3DScalarContainer.cc',
+                                 'src/TField3D_Grid.cc',
+                                 'src/TField3D_Gaussian.cc',
+                                 'src/TFieldContainer.cc',
+                                 'src/TField3D_IdealUndulator.cc',
+                                 'src/TField3D_UniformBox.cc',
+                                 'src/TFieldPythonFunction.cc',
+                                 'src/TParticleA.cc',
+                                 'src/TParticleBeam.cc',
+                                 'src/TParticleBeamContainer.cc',
+                                 'src/TParticleTrajectoryPoints.cc',
+                                 'src/TRandomA.cc',
+                                 'src/TSpectrumContainer.cc',
+                                 'src/TSurfaceOfPoints.cc',
+                                 'src/TSurfacePoint.cc',
+                                 'src/TSurfacePoints_3D.cc',
+                                 'src/TSurfacePoints_Rectangle.cc',
+                                 'src/TVector2D.cc',
+                                 'src/TVector3D.cc',
+                                 'src/TVector3DC.cc',
+                                 'src/TVector4D.cc',
+                                 'src/TOMATH.cc'],
+                      extra_compile_args=['-DCUDA', '-std=c++11', '-Wno-write-strings', '-Wall', '-O3', '-pedantic', '-fPIC', '-pthread'],
+                      libraries = ['cuda', 'cudart'],
+                      library_dirs = ['/usr/local/cuda/lib64', '/lib64', '/usr/lib64'],
+                      extra_objects = ['lib/OSCARSSR_Cuda.o']
+                     )
+
+
+moduleOSCARSTH = Extension('oscars.th',
+                      include_dirs = ['include'],
+                      sources = ['src/OSCARSTH.cc',
+                                 'src/OSCARSTH_Python.cc',
+                                 'src/TVector2D.cc',
+                                 'src/TVector3D.cc',
+                                 'src/TParticleA.cc',
+                                 'src/TParticleBeam.cc',
+                                 'src/TRandomA.cc',
+                                 'src/TParticleTrajectoryPoints.cc',
+                                 'src/TOMATH.cc'],
+                      extra_compile_args=['-DCUDA', '-std=c++11', '-Wno-write-strings', '-Wall', '-O3', '-pedantic', '-fPIC', '-pthread'],
+                      libraries = ['cuda', 'cudart'],
+                      library_dirs = ['/usr/local/cuda/lib64', '/lib64', '/usr/lib64'],
+                      extra_objects = ['lib/OSCARSSR_Cuda.o']
+                     )
+
+
+
+
+setup(
+  name="oscars",
+  version="1.36.03",
+  description = 'Open Source Code for Advanced Radiation Simulation',
+  author = 'Dean Andrew Hidas',
+  author_email = 'dhidas@bnl.gov',
+  url = 'http://oscars.bnl.gov/',
+  license = 'LICENSE.txt',
+  long_description = '''The OSCARS Package.''',
+  ext_modules = [moduleOSCARSSR, moduleOSCARSTH],
+  data_files=[('oscars', ['LICENSE.txt', 'COPYRIGHT.txt'])],
+  package_data = {'' : ['LICENSE.txt']},
+  package_dir = {'oscars': 'oscars'},
+  py_modules = ['oscars.plots_mpl', 'oscars.plots3d_mpl', 'oscars.parametric_surfaces']
+)
