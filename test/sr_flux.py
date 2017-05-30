1ac4ad58658fad377d7235db869185af197e4793
dhidas <dhidas@dhidas.com>
2017-05-30 12:07:34 -0400

    files to run tests

---
+++ test/sr_flux.py
@@ -0,0 +1,59 @@
+# To test the sr module flux functions and mpl plots
+
+# Import the OSCARS SR module
+import oscars.sr
+
+# Import basic plot utilities (matplotlib).  You don't need these to run OSCARS, but it's used here for basic plots
+from oscars.plots_mpl import *
+
+# Create a new OSCARS object
+osr = oscars.sr.sr()
+
+# Set default number of threads
+osr.set_nthreads_global(8)
+
+# Clear any existing fields (just good habit in notebook style) and add an undulator field
+osr.clear_bfields()
+osr.add_bfield_undulator(bfield=[0, 1, 0], period=[0, 0, 0.049], nperiods=21)
+
+# Just to check the field that we added seems visually correct
+
+# Setup beam similar to NSLSII
+osr.clear_particle_beams()
+osr.set_particle_beam(type='electron',
+                      name='beam_0',
+                      x0=[0, 0, -1],
+                      d0=[0, 0, 1],
+                      energy_GeV=3,
+                      current=0.500
+                     )
+
+# Set the start and stop times for the calculation
+osr.set_ctstartstop(0, 2)
+
+# Print info
+osr.print()
+
+
+# Calculate spectrum zoom
+flux = osr.calculate_flux_rectangle(plane='XY',
+                                    energy_eV=152,
+                                    width=[0.01, 0.01],
+                                    npoints=[21, 21],
+                                    translation=[0, 0, 30],
+                                    ofile='sr_flux.txt',
+                                    bofile='sr_flux.dat')
+plot_flux(flux, ofile='sr_flux.png')
+
+
+
+# Create a new OSCARS object
+osr = oscars.sr.sr()
+flux = osr.average_flux(ifiles=['sr_flux.txt'])
+plot_flux(flux)
+
+
+# Create a new OSCARS object
+osr = oscars.sr.sr()
+flux = osr.average_flux(bifiles=['sr_flux.dat'])
+plot_flux(flux)
