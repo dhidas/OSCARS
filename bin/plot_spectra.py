3e2e0d300a112cee11312e0e0683e5bbbef23d06
dhidas <dhidas@dhidas.com>
2017-08-02 10:51:55 -0400

    adding plotting tool

---
+++ bin/plot_spectra.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+import oscars.sr
+from oscars.plots_mpl import plot_spectra
+
+osr = oscars.sr.sr()
+
+
+spectra = []
+labels  = []
+
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
+    spectra.append(osr.average_spectra(ifiles=[sys.argv[i]]))
+    labels.append(i)
+
+
+plot_spectra(spectra, labels)
+
