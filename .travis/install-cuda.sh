2fb3ff163432ebce5510afe6ab6d4078eb524b67
dhidas <dhidas@dhidas.com>
2017-10-23 18:34:15 -0400

    testing

--- .travis/install-cuda.sh
+++ .travis/install-cuda.sh
@@ -4,7 +4,7 @@ travis_retry sudo dpkg -i cuda-7-0_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA}
 export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda
+travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
 travis_retry sudo apt-get clean
 export CUDA_HOME=/usr/local/cuda-${CUDA}
 export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
