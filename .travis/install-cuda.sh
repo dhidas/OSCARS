0681ee91e3b1adb3c60eeb253288bc5b5538d171
dhidas <dhidas@dhidas.com>
2017-10-23 18:38:45 -0400

    testing

--- .travis/install-cuda.sh
+++ .travis/install-cuda.sh
@@ -1,13 +1,14 @@
-export CUDA=7.0-28
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-7-0_${CUDA}_amd64.deb
-travis_retry sudo dpkg -i cuda-7-0_${CUDA}_amd64.deb
-travis_retry sudo apt-get update -qq
-export CUDA_APT=${CUDA}
-export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
-travis_retry sudo apt-get clean
-export CUDA_HOME=/usr/local/cuda-${CUDA}
-export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-export PATH=${CUDA_HOME}/bin:${PATH}
+export CUDA_VERSION=7.0-28
+
+
+wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb";
+sudo dpkg -i cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb;
+sudo apt-get update -qq;
+export CUDA_APT=${CUDA_VERSION%-*};
+export CUDA_APT=${CUDA_APT/./-};
+sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT};
+sudo apt-get clean;
+export CUDA_HOME=/usr/local/cuda-${CUDA_VERSION%%-*};
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH};
+export PATH=${CUDA_HOME}/bin:${PATH};
 
