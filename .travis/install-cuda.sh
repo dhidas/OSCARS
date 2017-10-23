2ceff4d5a503ef7aece28572ac6f62a4207a0bf4
dhidas <dhidas@dhidas.com>
2017-10-23 17:27:16 -0400

    testing

--- .travis/install-cuda.sh
+++ .travis/install-cuda.sh
@@ -1,10 +1,10 @@
-export CUDA=7.5-18
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+export CUDA=7.0-28
+travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/uda_${CUDA}_amd64.deb
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA}
 export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT}
+travis_retry sudo apt-get install -y cuda
 travis_retry sudo apt-get clean
 export CUDA_HOME=/usr/local/cuda-${CUDA}
 export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
