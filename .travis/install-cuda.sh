11c04a7d0d7cacbad971938bb497890ffb0892de
dhidas <dhidas@dhidas.com>
2017-10-23 20:02:38 -0400

    testing

--- .travis/install-cuda.sh
+++ .travis/install-cuda.sh
@@ -5,12 +5,11 @@ wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/
 sudo dpkg -c cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb;
 echo hi
 sudo dpkg -i cuda-repo-ubuntu1204_${CUDA_VERSION}_amd64.deb;
-cat ./etc/apt/sources.list.d/cuda.list
 sudo apt-get update -qq;
 export CUDA_APT=${CUDA_VERSION%-*};
 export CUDA_APT=${CUDA_APT/./-};
 #sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT};
-sudo apt-get install -y cuda;
+sudo apt-get install -y cuda cuda-7-0 cuda-runtime-7-0;
 sudo apt-get clean;
 export CUDA_HOME=/usr/local/cuda-${CUDA_VERSION%%-*};
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH};
