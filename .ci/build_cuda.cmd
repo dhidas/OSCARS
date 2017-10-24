088a8f34731c4f7d1d102adb8d9beacb88642ecd
dhidas <dhidas@dhidas.com>
2017-10-24 15:40:15 -0400

    testing

--- .ci/build_cuda.cmd
+++ .ci/build_cuda.cmd
@@ -14,7 +14,10 @@ exit /B 1
 )
 
 set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
+set PATH=C:\MinGW\bin;%PATH%
 
 nvcc -V
+
+dir "C:\MinGW\bin"
 
 make
