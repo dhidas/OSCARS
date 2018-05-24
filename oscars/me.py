cd0c810cc0d7c24865e26aa4a5bd2be71f8a5807
dhidas <dhidas@dhidas.com>
2018-05-24 08:56:13 -0400

    comments

--- oscars/me.py
+++ oscars/me.py
@@ -2,6 +2,10 @@ from oscars.fit import *
 import sys
 
 class SpecRange:
+    """A class to keep track of a spectrum in sections, compare it to others, mark when complete
+    and save ranges"""
+
+    # Defatult class variables
     range_eV = [0, 0]
     done = False
     flux = 0
@@ -14,13 +18,18 @@ class SpecRange:
         self.range_eV=range_eV
 
     def get_points_eV (self):
+        """Get the energy points in a spectrum with a pitch of 1 eV"""
+
         pp = []
         if not self.done:
             for i in range(self.range_eV[0], self.range_eV[1]+1):
                 pp.append(i)
         return pp
 
-    def spec_test (self, h):
+    def harmonics_test (self, h):
+        """Compare harmonics with the internally saved one.  If it satisfies all criteria, return is true.
+        Tests include flux, energy, and fwhm differences"""
+
         # Compare spectrum to previous
         flux_test = abs((self.flux - h[0]) / self.flux) < 0.01
         energy_test = abs(self.energy - h[1]) / self.energy < 0.01
@@ -29,6 +38,8 @@ class SpecRange:
         return flux_test and energy_test and fwhm_test
     
     def save_spectrum_range(self, sin):
+        """Save the spectrum range"""
+
         self.spectrum = []
         for s in sin:
             if s[0] >= self.range_eV[0] and s[0] <= self.range_eV[1]:
@@ -52,11 +63,16 @@ def get_points(sr):
 
 def calculate_harmonics_me (osr, obs=[0, 0, 30], nparticles=1000, niterations=10):
 
+
+    # Grab the single particle spectrum and harmonics for reference
     spectrum_se = osr.calculate_spectrum(obs=[0, 0, 30], energy_range_eV=[10, 30000])
     harmonics = find_odd_harmonics(spectrum_se, show=False)
-    
+
+    # For the summed spectrum from multi-electron sampling
     spectrum_summed = []
     specr = []
+
+    # Loop over harmonics and append SpecRanges given the FWHM
     for h in harmonics:
         specr.append(SpecRange([int(h[1] - 6*h[2]), int(h[1] + 3*h[2])]))
 
@@ -101,7 +117,7 @@ def calculate_harmonics_me (osr, obs=[0, 0, 30], nparticles=1000, niterations=10
                     print('did not see spectrum in ith spectrum')
 
 
-                if len(peak_i) != 0 and sr.spec_test(peak_i[0]):
+                if len(peak_i) != 0 and sr.harmonics_test(peak_i[0]):
                     sr.done = True
                     sr.save_spectrum_range(spectrum_summed)
                     sr.nparticles = total_particles
