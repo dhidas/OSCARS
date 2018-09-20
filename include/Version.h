14d754b2b2ecd7608f49a8ecf1bb3496b00e43f3
dhidas <dhidas@dhidas.com>
2018-09-20 10:59:42 -0400

    dev version to 3 for STL development with ray tracing

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 #ifndef GUARD_Version_h
 #define GUARD_Version_h
 
-#define OSCARS_VMAJOR 2
-#define OSCARS_VMINOR 1
-#define OSCARS_REVISION 9
-#define OSCARS_RELEASE NULL
+#define OSCARS_VMAJOR 3
+#define OSCARS_VMINOR 0
+#define OSCARS_REVISION 0
+#define OSCARS_RELEASE "dev0"
 
 #endif
