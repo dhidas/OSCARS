ac2b6fa63c56c684ce45669393e655af97c05330
Dean Hidas <dhidas@apcpu-master01.cs.nsls2.local>
2019-02-28 18:16:34 -0500

    working for lut

--- oscars/util.py
+++ oscars/util.py
@@ -48,8 +48,8 @@ def read_file_list_with_header (ifile, idir=None):
         
     Returns
     -------
-    file_list : list
-        List of [parameter, filename]s
+    mapping: mapping object
+        Mapping of parameters and filenames along with header infomation as members
     """
     
     # Directory where files are
@@ -75,6 +75,73 @@ def read_file_list_with_header (ifile, idir=None):
             fn = os.path.join(mydir, ' '.join(ls[1:]))
             
             mylist.append([pv, fn])
+    mymap = mapping()
+    mymap.mapping = mylist
+    mymap.format = iformat
+    mymap.rotations = rotations
+    mymap.translation = translation
+    mymap.scale = scale
+    return mymap
+    #return [mylist, iformat, rotations, translation, scale]
+
+
+
+
+
+def read_file_list_2d_with_header (ifile, idir=None):
+    """
+    read a list of parameters and filenames from a file.
+    
+    The file shal have a header of 4 lines:
+        iformat
+        rotations x y z
+        translation x y z
+        scale
+
+    The file format should be four columns separated by whitespace.
+    In the first column is the gap value as a floating point number
+    and in the second column the phase mode, thrd, the phase as a float,
+    and fourth column the file name.  Whitespace in the name is
+    unforgivably wrong, but technically allowed.
+    
+    Parameters
+    ----------
+    ifile : str
+        Full path to file containing the list of parameters and filenames
+        
+    idir : str
+        Path to directory where the files are contained if not in the 'ifile' directory
+        
+    Returns
+    -------
+    mapping: mapping object
+        Mapping of parameters and filenames along with header infomation as members
+    """
+    
+    # Directory where files are
+    mydir = os.path.dirname(ifile)
+    if idir is not None:
+        mydir = idir
+    
+    # List we will return
+    mylist=[]
+    with open(ifile, 'r') as fi:
+        iformat = fi.readline().rstrip().partition('#')[0]
+        rotations = list(map(float, fi.readline().partition('#')[0].split()))
+        translation = list(map(float, fi.readline().partition('#')[0].split()))
+        scale = list(map(float, fi.readline().partition('#')[0].split()))
+
+        for l in fi:
+            # Check for blank line or comment
+            if len(l.strip()) == 0 or l.startswith('#'):
+                continue
+
+            ls = l.split()
+            gap = float(ls[0])
+            phase = float(ls[2])
+            fn = os.path.join(mydir, ' '.join(ls[3:]))
+            
+            mylist.append([gap, phase, fn])
     mymap = mapping()
     mymap.mapping = mylist
     mymap.format = iformat
@@ -203,6 +270,97 @@ def read_file_list2(ifile, gap=None, phase_mode=None, phase=None, idir=None):
 
 
 
+def interpolate_file_list_2d (mapping, gap, phase, ofile, idir=None, tmpdir=None):
+    """
+    read a list of parameters and filenames from mapping.  Interpolates across phases for each gap to get
+    desired phase at each gap, then Interpolate across gaps to arrive at gap and phase, for a given mode
+    
+    
+    Parameters
+    ----------
+    mapping : mapping
+        Mapping containing format parameters and list of parameters with filenames
+
+    gap : float
+        Gap value of interest (must be exact match, not interpolated)
+
+    phase : float
+        Phase value
+
+    idir : str
+        Path to directory where the files are contained if not in the 'ifile' directory
+        
+    tmpdir: str
+        Name of temp directory to store temporary files.  If not given OSCARS will create oen for you and delete it.
+        
+    ofile: str
+        Name of output file.  Output file is in the same units and format of input files
+        
+    Returns
+    -------
+    ofile_name : str
+        Name of the output file
+    """
+
+    if ofile is None:
+        raise ValueError('Must specify output file as ofile')
+
+    # Set temporary directory for interpolating results
+    tmpdir_actual = tmpdir
+    if tmpdir is None:
+        tmpdir_actual = '.OSCARS_tmp_' + str(uuid.uuid4())
+
+    if os.path.exists(tmpdir_actual):
+        raise
+
+    os.makedirs(tmpdir_actual)
+    
+    # oscars.sr object for interpolation
+    osr = oscars.sr.sr(gpu=0, nthreads=1)
+
+    # Store all phases that a gap has
+    phases_at_gap = dict()
+    
+    # List we will return
+    mylist=[]
+    for ls in mapping.mapping:
+        if len(ls) < 3:
+            continue
+
+        g  = ls[0]
+        ph = ls[1]
+        fn = ls[2]
+    
+        if g in phases_at_gap:
+            phases_at_gap[g].append([ph, fn])
+        else:
+            phases_at_gap[g] = [[ph, fn]]
+
+    new_mapping = []
+
+
+    for key in phases_at_gap:
+        tmp_file_name = tmpdir_actual + '/gap' + str(key) + '.dat'
+        new_mapping.append([key, tmp_file_name])
+
+        osr.clear_bfields()
+        osr.add_bfield_interpolated(mapping=phases_at_gap[key], iformat=mapping.format, parameter=phase, ofile=tmp_file_name)
+
+    osr.clear_bfields()
+    osr.add_bfield_interpolated(mapping=new_mapping, iformat=mapping.format, parameter=gap, ofile=ofile)
+
+    for pair in new_mapping:
+        os.remove(pair[1])
+
+    if tmpdir is None:
+        os.rmdir(tmpdir_actual)
+            
+    return ofile
+
+
+
+
+
 def read_file_list_interpolate_2d(ifile, iformat, ofile, gap, phase_mode, phase, idir=None, tmpdir=None):
     """
     read_file_list_interpolate_2d(ifile, iformat, ofile, gap, phase_mode, phase [, idir=None, tmpdir=None])
@@ -291,11 +449,12 @@ def read_file_list_interpolate_2d(ifile, iformat, ofile, gap, phase_mode, phase,
                 continue
 
             if g in phases_at_gap:
-               phases_at_gap[g].append([ph, fn])
+                phases_at_gap[g].append([ph, fn])
             else:
                 phases_at_gap[g] = [[ph, fn]]
 
     new_mapping = []
+
 
     for key in phases_at_gap:
         tmp_file_name = tmpdir_actual + '/gap' + str(key) + '.dat'
